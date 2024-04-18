c7cbf2bef63bd38f5e9cdc4ce1f4d4e266822ce3
Josep Viciana <emmgfx@gmail.com>
2024-04-18 14:12:42 +0200

    Adds link to source code

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,11 @@ export default function RootLayout({ children }) {
       <body className="flex flex-col h-full">
         <Header />
         {children}
+        <footer className="text-xs p-2 text-center">
+          <a href="https://github.com/3Data/broker-nextjs" target="_blank">
+            See source code on GitHub
+          </a>
+        </footer>
       </body>
     </html>
   );
