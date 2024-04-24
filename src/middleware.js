b69e6b611fd996ef3fc7c875edea2b27381a8708
Josep Viciana <emmgfx@gmail.com>
2024-04-24 11:37:17 +0200

    Basic auth

---
+++ src/middleware.js
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const basicAuth = request.headers.get("authorization");
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+    const validUser = process.env.BASIC_AUTH_USER;
+    const validPassWord = process.env.BASIC_AUTH_PASSWORD;
+    if (user === validUser && pwd === validPassWord) return NextResponse.next();
+  }
+  return NextResponse.rewrite(new URL("/api/auth", request.url));
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
